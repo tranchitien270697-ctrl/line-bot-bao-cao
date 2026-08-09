4923957529568d02fbbde422ab18d1dc821b29c7
tranchitien270697-ctrl <tranchitien270697@gmail.com>
2026-08-09 16:17:10 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,20 @@
+// config.js
+module.exports = {
+  TRIGGER_KEYWORD: 'báo cáo',
+
+      FRESH_INDUSTRIES: ['Rau Củ Quả CL', 'Cá (Hải sản)', 'Thịt', 'Trứng', 'Trái cây'],
+
+      DAYS_IN_TARGET_MONTH: 31,
+
+      LABEL_PREV_MONTH: 'Tháng 7',
+      LABEL_CURR_MONTH: 'Tháng 8 (dự kiến)',
+
+      GOOGLE_SHEET_ID: '1wQlsrIsXTAwWByZwmfOs3f4Hsuh9TmLdWHg_1ECMttE',
+
+      TAB_GIDS: {
+    revenuePrevMonth: '0',
+          revenueCurrMonth: '1560618412',
+          volumePrevMonth: '1085666896',
+          volumeCurrMonth: '594018980',
+      },
+};
