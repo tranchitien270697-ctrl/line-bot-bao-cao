81fc9d90e2ead805787b154afd1115119f70012a
tranchitien270697-ctrl <tranchitien270697@gmail.com>
2026-08-09 18:05:23 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // config.js
 module.exports = {
   TRIGGER_KEYWORD: 'báo cáo',
-      ACTUAL_KEYWORD: 'doanh thu hien tai',
+        ACTUAL_KEYWORD: 'doanh thu hiện tại',
 
       FRESH_INDUSTRIES: ['Rau Củ Quả CL', 'Cá (Hải sản)', 'Thịt', 'Trứng', 'Trái cây'],
 
